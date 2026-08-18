5e8bb7b35b5ede8403f521645da90b518e02b764
Inlineichsan <ichsan.rusdianto@unipin.com>
2026-08-18 11:53:18 +0700

    feat: change url

--- routers/bookRouter.go
+++ routers/bookRouter.go
@@ -11,8 +11,8 @@ const bookID = "/book/:bookID"
 func StartServer() *gin.Engine {
 	router := gin.Default()
 
-	router.POST("/bioskops", controllers.InsertBook)
-	router.GET("/bioskops", controllers.GetBooks)
+	router.POST("/book", controllers.InsertBook)
+	router.GET("/book", controllers.GetBooks)
 	router.PUT(bookID, controllers.UpdateBook)
 	router.DELETE(bookID, controllers.DeleteBook)
 
